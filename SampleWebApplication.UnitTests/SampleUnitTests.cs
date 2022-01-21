426cc41cf6a17e01df771018df9f1d442c7cba75
Muhammed Tungeker <ahmedt84@hotmail.com>
2022-01-21 17:28:57 -0500

    Changes

--- SampleWebApplication.UnitTests/SampleUnitTests.cs
+++ SampleWebApplication.UnitTests/SampleUnitTests.cs
@@ -28,7 +28,7 @@ namespace SampleWebApplication.UnitTests
         {
             var controller = new HomeController();
             ActionResult result = controller.Contact();
-            Assert.AreEqual("Your contact.", controller.ViewBag.Message);
+            Assert.AreEqual("Your contact pagess.", controller.ViewBag.Message);
         }
     }
 }
